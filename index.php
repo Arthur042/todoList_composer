85c43e6b1554a09e24ce8e3dc90b4be01629e6b8
Arthur042 <arthur.andre042@gmail.com>
2022-07-13 09:45:34 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, natus.</li>
+        <li>Lorem ipsum dolor sit amet.</li>
+        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam provident, eos quis quidem perferendis nobis voluptatem fugit velit odit molestias quibusdam cumque voluptas id nesciunt dolorem necessitatibus corrupti harum rem.</li>
+        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis, blanditiis?</li>
+        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis, blanditiis?</li>
+    </ul>
+</body>
+</html>
